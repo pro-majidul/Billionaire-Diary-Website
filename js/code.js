f58cf122f1e300eaa05845cd2dc06e1709408c66
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-10-13 22:26:46 +0600

    daynamically show 10 biliniors details

--- js/code.js
+++ js/code.js
@@ -1,2 +1,33 @@
 const htmlTag = document.getElementsByTagName("html")[0];
 htmlTag.setAttribute("data-theme", "light");
+
+
+// normaly fetch 10 billionoir and randomly show them 
+const most10bilinior = () => {
+    fetch('https://forbes400.onrender.com/api/forbes400?limit=10')
+        .then(res => res.json())
+        .then(data => DisplayBilinior(data))
+}
+
+// randomly show them
+const DisplayBilinior = (items) => {
+    const miliniorField = document.getElementById('milinior-field');
+    items.forEach(element => {
+        const div = document.createElement('div');
+        div.className =" flex py-3 justify-between gap-5"
+        div.innerHTML = `
+              <div class=" flex gap-3">
+                     <p class="font-semibold">${element.personName}</p>
+                     <img class="rotate-90" src="./images/icons/Group 8.png" alt="">
+                 </div>
+                 <p class="font-semibold">${element.countryOfCitizenship}</p>
+                 <p class="font-semibold">${element.industries}</p>
+                  <p class="font-semibold">${element.rank}</p>
+                  <p class="font-semibold">$ ${element.finalWorth}</p>
+        
+        `
+        miliniorField.appendChild(div)
+    });
+}
+
+most10bilinior()
